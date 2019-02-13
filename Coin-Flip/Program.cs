92796428846222acc27272c6d5a37d40a09d2670
Pi-Cla <pirateclip@protonmail.com>
2019-02-13 13:35:50 -0700

    Part 1

--- Coin-Flip/Program.cs
+++ Coin-Flip/Program.cs
@@ -6,7 +6,28 @@ namespace Coin_Flip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            //Query the user
+            Console.Write("Enter you geuss, Heads or Tails (H or T)");
+            userGuess = Console.ReadLine();
+
+            //get a random number for the coin flip
+            coin = rng.Next(0,2);
+
+            if (coin == 0 && userGuess == "T"){
+                Console.WriteLine("The coin flip was Tails, you win!");
+            } else if (coin == 1 && userGuess == "H"){
+                Console.WriteLine("The coinf lip was Heads, you win!");
+            } else {
+                if (coin == 0){
+                    Console.WriteLine("The coin flip was Tails, you lose!");
+                } else {
+                    Console.WriteLine("The coin flip was Heads, you lose!");
+                }
+            }
         }
     }
 }
